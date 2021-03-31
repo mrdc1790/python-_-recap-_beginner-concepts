c294c188d39b42866c7b2c8e6d58dff336a9577c
apollac4 <apollac4@emich.edu>
2021-03-31 19:32:33 -0400

    Finished hangman

--- little projects (easy diff)/04-hangman/hangman.py
+++ little projects (easy diff)/04-hangman/hangman.py
@@ -15,11 +15,13 @@ def hangman():
 	alphabet = set(string.ascii_uppercase)
 	used_letters = set()				# what the user has guessed
 
+	lives = len(word)
+
 	# getting user input
-	while len(word_letters) > 0:
+	while len(word_letters) > 0 and lives > 0:
 		# letters used
 		# ' '.joing(['a','b','cd']) --> 'a b cd'
-		print("You have used these letters: ", " ".join(used_letters))
+		print("You have ",lives,"lives. You have used these letters: ", " ".join(used_letters))
 		# what current word is (ie. W - R D)
 		word_list = [letter if letter in used_letters else '-' for letter in word]
 		print("Current word: ", " ".join(word_list))
@@ -28,14 +30,20 @@ def hangman():
 			used_letters.add(user_letter)
 			if user_letter in word_letters:
 				word_letters.remove(user_letter)
+			else:
+				lives = lives -1 
+				print("Letter is not in word.")
 
 		elif user_letter in used_letters:
 			print("You have already used that character. Please try again.")
 
 		else:
 			print("Invalid character")
-
-	print("Yay! You guessed the word: ",word,"!!!")		
+	#gets here when len(word_letters) == 0 OR when lives = 0
+	if lives == 0:
+		print("You died, sorry. The word was ",word,".")
+	else:
+		print("Yay! You guessed the word: ",word,"!!!")		
 
 if __name__ == "__main__":
 	hangman()
